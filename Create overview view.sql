927410334233db8ff47fdbd37a2724ceeab9b2e8
Luka Wilson-Green <lukawg@icloud.com>
2024-10-29 16:21:12 +0000

    Update type of join

--- Create overview view.sql
+++ Create overview view.sql
@@ -4,8 +4,8 @@ SELECT [CW1].[Trail].[Trail_ID], [CW1].[Trail].[Title], [CW1].[Trail].[Rating], 
     [CW1].[Trail].[Type], [CW1].[Trail].[Description], [CW1].[Tag].[Tag_name]
 
 FROM [CW1].[Trail]
-JOIN [CW1].[Trail_tag]
+LEFT JOIN [CW1].[Trail_tag]
 ON [CW1].[Trail].[Trail_ID] = [CW1].[Trail_tag].[Trail_ID]
 
-JOIN [CW1].[Tag]
+LEFT JOIN [CW1].[Tag]
 ON [CW1].[Trail_tag].[Tag_ID] = [CW1].[Tag].[Tag_ID];
